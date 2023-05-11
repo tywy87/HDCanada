a86f1bd356b6d3e67adf46fc544948e050aad620
Tyler Wyles <tylerwyles@gmail.com>
2023-05-11 16:41:52 -0400

    Added files for organization

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import '../styles/global.css'
+import '../styles/header.css'
+
+function Header() {
+    return(
+        <nav>
+
+        </nav>
+        <div className="wrapper">
+            <header className="header">
+
+            </header>
+        </div>
+    );
+}
+
+export default Header;
